d409326744d62cb370fbd433734eceedc256063d
Antonia <93054555+AntoniaPache@users.noreply.github.com>
2026-02-09 16:12:44 -0300

    DTO

---
+++ src/main/java/com/pache/task_api/DTO/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.pache.task_api.DTO;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+	
+	@NotBlank(message = "Username is required")
+    private String username;
+    
+    @NotBlank(message = "Password is required")
+    private String password;
+
+}
